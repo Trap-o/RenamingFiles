9cc42193d9974a9364715b45817bf71b8ec6382e
Trap <yatsiks69@gmail.com>
2024-06-27 16:10:31 +0300

    test

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -9,7 +9,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Path = System.IO.Path;
 
 namespace RandomNamesWithUI
