b58be9bbe77fa70d0409750e69f5b2d99145b029
Trap <yatsiks69@gmail.com>
2024-07-11 16:01:41 +0300

    Add message window and info about app Small design fixes Change gitignore

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,16 @@
 ﻿using Microsoft.Win32;
+using System.Diagnostics.Metrics;
 using System.IO;
+using System.Runtime.Intrinsics.X86;
 using System.Windows;
+using static System.Net.WebRequestMethods;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Windows.Controls.Primitives;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+using System.Xml.Linq;
 using Path = System.IO.Path;
+using File = System.IO.File;
 
 namespace RandomNamesWithUI
 {
@@ -138,11 +147,32 @@ namespace RandomNamesWithUI
                 testLabel.Content = $"Failed to rename file(s) in folder: {ex.Message}";
             }
         }
-
         private async Task ReturnToDefaultLabelText()
         {
             await Task.Delay(5000);
             testLabel.Content = "Choose action";
+        }
+
+        private void InfoButton_Click(object sender, RoutedEventArgs e)
+        {
+            string caption = "Info";
+            string messageBoxText = "First, You need to select the object to rename form drop - down list: " +
+                "\"Select folder(s)\" – select one or more folders and rename ALL files in the folder(s); " +
+                "\"Select file(s)\" – select one or more files in specific directory and rename ONLY THE SELECTED file(s). " +
+                "If You don't do this, the message \"Select what you want to rename from list!\" displayes. " +
+                "\n\nAfter that, You choose the type of renaming: " +
+                "generate random name(s) for file(s) with certain letters and numbers or give them similar specific name(s), " + 
+                "that You enter yourself(like \"photo_0, photo_1...\", etc.). " +
+                "\nFor the first variant, You just need to click the \"Generate name\" button and then select folder(s) or file(s) to rename " +
+                "(depending on what you select in drop - down list)." +
+                "\nFor the second variant, firstly, You need to click the \"Change name manually\" button, " +
+                "secondly, enter a new name for file(s) and click the \"Continue\" button, thirdly, select folder(s) or file(s) to rename " +
+                "(depending on what you select in drop - down list). Finally, You can see results of files\' renaming." +
+                "\n\nThe app was developed by Trap_o out of personal need (and as a small portfolio project). " +
+                "Please, write any suggestions (new features, app design, bugs, code improvements, etc.) in \"Discussions\" section (English or Ukrainian).";
+            MessageBoxButton button = MessageBoxButton.OK;
+            MessageBoxImage icon = MessageBoxImage.Asterisk;
+            MessageBox.Show(messageBoxText, caption, button, icon);
         }
     }
 }
